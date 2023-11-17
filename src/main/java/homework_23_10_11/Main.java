d74999013603395a67a8925c6ca04fe711bc0227
Bogoliubova.Tetiana <bogol.tatyana@gmail.com>
2023-11-17 19:38:01 +0100

    HW_10.11.23

---
+++ src/main/java/homework_23_10_11/Main.java
@@ -0,0 +1,8 @@
+package homework_23_10_11;
+
+public class Main {
+    public static void main(String[] args) {
+        WorkWithStreams workWithStreams = new WorkWithStreams();
+        workWithStreams.run();
+    }
+}
